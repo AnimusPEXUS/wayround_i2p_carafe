16b773f2de97ae8b214e60e59ab6c45de4673a34
Alexey V Gorshkov <animus@wayround.org>
2014-11-28 11:49:13 +0300

    fixes

--- org/wayround/carafe/carafe.py
+++ org/wayround/carafe/carafe.py
@@ -67,27 +67,43 @@ class ResponseStartWrapper:
             #status = bytes(status, self._output_encoding)
 
         #print('response_headers: {}'.format(response_headers))
-        for i in range(len(response_headers)):
+        # for i in range(len(response_headers)):
 
-            iv = response_headers[i]
+         #   iv = response_headers[i]
             #print('iv: {}'.format(iv))
 
-            iv0t = type(iv[0])
-            iv1t = type(iv[1])
+          #  iv0t = type(iv[0])
+           # iv1t = type(iv[1])
 
-            if iv0t == str or iv1t == str:
-                iv0 = iv[0]
-                iv1 = iv[1]
+           # if iv0t == str or iv1t == str:
+           #     iv0 = iv[0]
+           #     iv1 = iv[1]
 
-                if iv0t == str:
-                    iv0 = bytes(iv0, self._output_encoding)
+           #     if iv0t == str:
+           #         iv0 = bytes(iv0, self._output_encoding)
 
-                if iv1t == str:
-                    iv1 = bytes(iv1, self._output_encoding)
+           #     if iv1t == str:
+           #         iv1 = bytes(iv1, self._output_encoding)
 
-                response_headers[i] = iv0, iv1
+            #    response_headers[i] = iv0, iv1
 
-        return self._response_start(status, response_headers, exc_info)
+        return ResponseStartResultWrapper(
+            self._response_start(status, response_headers, exc_info),
+            self._output_encoding
+            )
+
+
+class ResponseStartResultWrapper:
+
+    def __init__(self, response_start_result, output_encoding='utf-8'):
+        self._response_start_result = response_start_result
+        self._output_encoding = output_encoding
+        return
+
+    def __call__(self, data):
+        if isinstance(data, str):
+            data = bytes(data, self._output_encoding)
+        return self._response_start_result
 
 
 class Carafe:
