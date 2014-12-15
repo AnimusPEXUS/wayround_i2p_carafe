c49bed2b0205ffc9a05394adf096bd67f97cef11
Alexey V Gorshkov <animus@wayround.org>
2014-12-15 18:43:41 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name='org_wayround_carafe',
+    version='0.1',
+    description='micro web-framefork for wsgi',
+    author='Alexey Gorshkov',
+    author_email='animus@wayround.org',
+    url='https://github.com/AnimusPEXUS/org_wayround_carafe',
+    packages=[
+        'org.wayround.carafe'
+        ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+        ]
+    )
